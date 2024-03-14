c06eebc54d38395f0445311d192c27d8f5901806
Debraj Sarkar <debrajsarkarsiliguri123@gmail.com>
2024-03-15 01:40:29 +0530

    login page

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:campus_grub_official/auth/fire_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,14 +9,55 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          ElevatedButton(
+      backgroundColor: Color.fromRGBO(227, 5, 72, 1),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.contain,
+            alignment: Alignment.topLeft,
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            Text(
+              'Campus Grub',
+              style: GoogleFonts.carterOne(
+                textStyle: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Image.asset('assets/burger_croped.png'),
+            Text(
+              'Waiting In Queues\nNo More',
+              style: GoogleFonts.lato(
+                textStyle: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SignInButton(
+              Buttons.google,
               onPressed: () {
                 FirebaseService.signInwithGoogle(context);
               },
-              child: Text('sign in with google '))
-        ],
+            ),
+            SignInButton(
+              Buttons.apple,
+              text: "Sign up with Google",
+              onPressed: () {},
+            )
+          ],
+        ),
       ),
     );
   }
